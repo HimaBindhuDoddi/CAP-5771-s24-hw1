a295ca41187a8404e41d1c34e54a1d593b4afd18
HimaBindhuDoddi <156735714+HimaBindhuDoddi@users.noreply.github.com>
2024-01-29 22:56:46 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -8,7 +8,7 @@ def question6_1():
 
 def question6_2():
     answer = {
-        'equal_width': ['change', 9],
+        'equal_width': ['No change', 9],
         'equal_frequency': ['No change', 9]
     }
     return answer
