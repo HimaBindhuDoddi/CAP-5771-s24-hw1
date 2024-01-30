4a09f292604b9b8d050a8b432785f257c45b8280
HimaBindhuDoddi <156735714+HimaBindhuDoddi@users.noreply.github.com>
2024-01-29 22:22:05 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,19 +1,23 @@
 def question5_1():
     answer = {
-            'bin1': [1,2,3,4,5]
+            'bin1': [1,2,3,4,5],
             'bin2': [6,7,8],
             'bin3': [9]
     }
     return answer
 
 def question5_2():
-    'bin1': [1,2,3]
-    'bin2': [4,5,6],
-    'bin3': [7,8,9]
+    answer = {
+        'bin1': [1,2,3],
+        'bin2': [4,5,6],
+        'bin3': [7,8,9]
+    }
     return answer
 
 def question5_3():
-    'bin1': [1,2,3,4]
-    'bin2': [5,6,7],
-    'bin3': [8,9]
+    answer = {
+        'bin1': [1,2,3,4],
+        'bin2': [5,6,7],
+        'bin3': [8,9]
+    }
     return answer 
