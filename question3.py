19f525a40487dd1889032146aadd1abfea8ae8fe
HimaBindhuDoddi <156735714+HimaBindhuDoddi@users.noreply.github.com>
2024-01-29 22:30:36 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,5 +1,5 @@
 def question3_1():
-    answer = ['continous', 'quantitative', 'ratio']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question3_2():
@@ -11,7 +11,7 @@ def question3_3():
     return answer 
 
 def question3_4():
-    answer = ['continous', 'quantitative', 'ratio']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question3_5():
@@ -23,7 +23,7 @@ def question3_6():
     return answer
 
 def question3_7():
-    answer = ['continous', 'quantitative', 'interval']
+    answer = ['continuous', 'quantitative', 'interval']
     return answer
 
 def question3_8():
