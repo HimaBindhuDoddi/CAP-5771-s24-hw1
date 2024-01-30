ab68f369cd7fb29ac67f3996432929fa670fdb74
HimaBindhuDoddi <156735714+HimaBindhuDoddi@users.noreply.github.com>
2024-01-29 21:34:22 -0500

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,18 +1,22 @@
 def question9_1():
-    """
-    answer = string'
-    """
+   
+    answer = 'SMC’'
+   
     return answer
 
 def question9_2():
+    answer ='Jaccard'
     return answer
 
 def question9_3():
+    answer ='Euclidean'
     return answer 
 
 def question9_4():
+    answer ='Cosine Similarity'
     return answer 
 
 def question9_5():
+    answer ='Euclidean'
     return answer 
 
